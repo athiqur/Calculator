4f6a5bf44cb70ec530eb328a883cde6e60387780
Athiqur <athiqur@DESKTOP-GGE2S2B.localdomain>
2022-11-16 16:24:25 +0530

    Perform arithmetic operation

---
+++ webcalculator/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Number():
+    ZERO = 0
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
+
+class CalcOperatorSymbols():
+    PLUS = '+'
+    MINUS = '_'
+    MUL = 'x'
+    DIV = '/'
+    DOT = '.'
+    MODULUS = '%'
+    EQUAL = '='
+
---
+++ webcalculator/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+class Number():
+    ZERO = 0
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
+
+class CalcOperatorSymbols():
+    PLUS = '+'
+    MINUS = '_'
+    MUL = 'x'
+    DIV = '/'
+    DOT = '.'
+    MODULUS = '%'
+    EQUAL = '='
+
+def index(request):
+    return render(request, 'index.html',{'num' : Number, 'operator' : CalcOperatorSymbols})
